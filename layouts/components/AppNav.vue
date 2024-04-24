db06dcaac4b09bd8b28d9f0df6f8832fd4ca9933
Luke Policinski <luke@lukepolo.com>
2024-04-24 14:51:59 -0400

    wip

--- layouts/components/AppNav.vue
+++ layouts/components/AppNav.vue
@@ -54,6 +54,9 @@ import BreadCrumbs from "~/components/BreadCrumbs.vue";
           {{ me.player.name }}
         </span>
       </nuxt-link>
+      <div @click="linkDiscord" v-if="!me.discord_id">
+        Link Discord
+      </div>
     </nav>
   </aside>
 
@@ -122,6 +125,13 @@ export default {
         },
       ],
     };
+  },
+  methods: {
+    linkDiscord(){
+      window.location = `https://5stack.gg/auth/discord?redirect=${encodeURIComponent(
+          window.location.toString(),
+      )}`;
+    }
   },
   computed: {
     me() {

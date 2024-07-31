ea0e9873d04e605e6b16862ea11e7b4b87641087
Luke Policinski <Luke@LukePOLO.com>
2024-07-30 21:49:57 -0400

    chore: pwa testing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,6 +24,7 @@ export default defineNuxtConfig({
       name: '5stack',
       short_name: '5stack',
       icons: [
+<<<<<<< Updated upstream
         // {
         //   src: '/assets/favicon/android-chrome-96x96.png',
         //   sizes: '96x96',
@@ -40,6 +41,24 @@ export default defineNuxtConfig({
         //   type: 'image/png',
         //   purpose: 'any'
         // },
+=======
+        {
+          src: '/favicon/64.png',
+          sizes: '64x64',
+          type : 'image/png'
+        },
+        {
+          src: '/favicon/192.png',
+          sizes: '192x192',
+          type : 'image/png'
+        },
+        {
+          src: '/favicon/512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any'
+        },
+>>>>>>> Stashed changes
       ],
       theme_color: '#000000',
       background_color: '#000000',

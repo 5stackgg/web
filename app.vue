1acb15ef647335891b1f2cb9dcff4fa0726c24e0
Luke Policinski <Luke@LukePOLO.com>
2024-07-30 21:15:29 -0400

    wip

--- app.vue
+++ app.vue
@@ -55,30 +55,7 @@ export default {
       title: "5stack",
       meta: [{ name: "description", content: "5stack.gg" }],
       link: [
-        {
-          rel: "apple-touch-icon",
-          sizes: "180x180",
-          href: "/favicon/apple-touch-icon.png",
-        },
-        {
-          rel: "icon",
-          type: "image/png",
-          sizes: "32x32",
-          href: "/favicon/favicon-32x32.png",
-        },
-        {
-          rel: "icon",
-          type: "image/png",
-          sizes: "16x16",
-          href: "/favicon/favicon-16x16.png",
-        },
-        { rel: "manifest", href: "/favicon/manifest.json" },
-        {
-          rel: "mask-icon",
-          href: "/favicon/safari-pinned-tab.svg",
-          color: "#48acf0",
-        },
-        { rel: "shortcut icon", href: "/favicon/favicon.ico" },
+      
       ],
     });
   },

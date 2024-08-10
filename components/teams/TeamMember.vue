92dacf2cf9d787612b240d0a56203c3c8db81bfb
Luke Policinski <Luke@LukePOLO.com>
2024-08-10 14:44:24 -0400

    wip

--- components/teams/TeamMember.vue
+++ components/teams/TeamMember.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
-import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@/components/ui/command'
-import {ChevronDownIcon} from "lucide-vue-next";
-import {Popover, PopoverContent, PopoverTrigger} from "~/components/ui/popover";
-import {Avatar, AvatarFallback, AvatarImage} from "~/components/ui/avatar";
-import {Button} from "~/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import { ChevronDownIcon } from "lucide-vue-next";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "~/components/ui/popover";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+import { Button } from "~/components/ui/button";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -14,7 +25,7 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
   AlertDialogTrigger,
-} from '@/components/ui/alert-dialog'
+} from "@/components/ui/alert-dialog";
 </script>
 
 <template>
@@ -25,7 +36,7 @@ import {
     </Avatar>
     <div>
       <p class="text-sm font-medium leading-none">
-        {{ member.player.name }} <pre>{{ memberRole }}</pre>
+        {{ member.player.name }} <badge>{{ memberRole }}</badge>
       </p>
       <p class="text-sm text-muted-foreground">
         {{ member.player.steam_id }}
@@ -45,26 +56,41 @@ import {
         <CommandList>
           <CommandEmpty>No roles found.</CommandEmpty>
           <CommandGroup>
-            <CommandItem :value="role.value" class="flex flex-col items-start px-4 py-2" v-for="role of roles">
-              <p>{{role.value }}</p>
+            <CommandItem
+              :value="role.value"
+              class="flex flex-col items-start px-4 py-2"
+              v-for="role of roles"
+            >
+              <p>{{ role.value }}</p>
               <p class="text-sm text-muted-foreground">
                 {{ role.description }}
               </p>
             </CommandItem>
 
-            <CommandItem :value="false" class="flex flex-col items-start px-4 py-2">
+            <CommandItem
+              :value="false"
+              class="flex flex-col items-start px-4 py-2"
+            >
               <AlertDialog>
-                <AlertDialogTrigger @click.stop>Remove From Team</AlertDialogTrigger>
+                <AlertDialogTrigger @click.stop
+                  >Remove From Team</AlertDialogTrigger
+                >
                 <AlertDialogContent>
                   <AlertDialogHeader>
-                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                    <AlertDialogTitle
+                      >Are you absolutely sure?</AlertDialogTitle
+                    >
                     <AlertDialogDescription>
-                      This will remove {{ member.player.name }} ({{ member.player.steam_id }}) from the team.
+                      This will remove {{ member.player.name }} ({{
+                        member.player.steam_id
+                      }}) from the team.
                     </AlertDialogDescription>
                   </AlertDialogHeader>
                   <AlertDialogFooter>
                     <AlertDialogCancel>Cancel</AlertDialogCancel>
-                    <AlertDialogAction @click.stop="removeMember">Continue</AlertDialogAction>
+                    <AlertDialogAction @click.stop="removeMember"
+                      >Continue</AlertDialogAction
+                    >
                   </AlertDialogFooter>
                 </AlertDialogContent>
               </AlertDialog>
@@ -83,7 +109,9 @@ import {
         <AlertDialogHeader>
           <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
           <AlertDialogDescription>
-            This cancel the invite to {{ member.player.name }} ({{ member.player.steam_id }}).
+            This cancel the invite to {{ member.player.name }} ({{
+              member.player.steam_id
+            }}).
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
@@ -96,8 +124,8 @@ import {
 </template>
 
 <script lang="ts">
-import {generateMutation} from "~/graphql/graphqlGen";
-import type {e_team_roles_enum} from "~/generated/zeus";
+import { generateMutation } from "~/graphql/graphqlGen";
+import type { e_team_roles_enum } from "~/generated/zeus";
 
 interface Role {
   value: string;
@@ -111,7 +139,7 @@ interface Member {
     name: string;
     steam_id: string;
     avatar_url: string;
-  }
+  };
 }
 
 export default {
@@ -126,28 +154,28 @@ export default {
     },
     isInvite: {
       required: true,
-    }
+    },
   },
   data() {
     return {
       memberRole: undefined,
-    }
+    };
   },
   watch: {
-    ["member.role"] : {
+    ["member.role"]: {
       immediate: true,
       handler(role) {
         this.memberRole = role;
-      }
+      },
     },
     memberRole: {
       handler(role) {
-        if(role) {
+        if (role) {
           this.publishRole();
           return;
         }
-      }
-    }
+      },
+    },
   },
   methods: {
     async removeMember() {
@@ -190,7 +218,7 @@ export default {
               pk_columns: {
                 team_id: this.member.team_id,
                 player_steam_id: this.member.player.steam_id,
-              }
+              },
             },
             {
               __typename: true,
@@ -198,7 +226,7 @@ export default {
           ],
         }),
       });
-    }
-  }
-}
+    },
+  },
+};
 </script>

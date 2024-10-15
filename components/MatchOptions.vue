629172acc85cc99ecfadce3ca5d143b79e2d54f1
Luke Policinski <Luke@LukePOLO.com>
2024-10-15 16:07:54 -0400

    bug: fix import

--- components/MatchOptions.vue
+++ components/MatchOptions.vue
@@ -14,6 +14,7 @@ import {
   CollapsibleTrigger,
   CollapsibleContent,
 } from "~/components/ui/collapsible";
+import FiveStackToolTip from "./FiveStackToolTip.vue";
 </script>
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 gap-6">

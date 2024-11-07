aba9154bba67fc5a385805a7d9792f3d0f8cd992
Luke Policinski <Luke@LukePOLO.com>
2024-11-07 09:17:56 -0500

    chore: server seo

--- app.vue
+++ app.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
 import { polyfillCountryFlagEmojis } from "country-flag-emoji-polyfill";
 
-const title = "5Stack - Counter-Strike Management System";
+const title = "5Stack | Counter-Strike Management System";
 const description = "A Comprehensive Panel for Managing Servers, Matches, and Tournaments";
 
 useSeoMeta({
+  title: title,
+  ogTitle: title,
+  description: description,
+  ogDescription: description,
+  ogImage: 'https://5stack.gg/_ipx/_/favicon/512.png',
+  twitterCard: description,
+})
+
+useServerSeoMeta({
+  robots: 'index, follow',
   title: title,
   ogTitle: title,
   description: description,

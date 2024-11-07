9790386cd448c9328579e5365891b9f70f83831a
Luke Policinski <Luke@LukePOLO.com>
2024-11-07 10:22:09 -0500

    chore: disable gst

--- app.vue
+++ app.vue
@@ -5,22 +5,7 @@ const title = "5Stack | Counter-Strike Management System";
 const description = "A Comprehensive Panel for Managing Servers, Matches, and Tournaments";
 const url = `https://${useRuntimeConfig().public.webDomain}`
 
-useSeoMeta({
-  title: title,
-  description: description,
-  siteName: '5Stack',
-
-  ogLocale: 'en',
-  ogType: 'website',
-  
-  ogTitle: title,
-  ogSiteName: '5Stack',
-
-  ogUrl: url,
-  ogImage: `${url}/_ipx/_/favicon/512.png`,
-})
-
-useServerSeoMeta({
+definePageMeta({
   robots: 'index, follow',
   title: title,
   description: description,

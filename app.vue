20e485683b1e830d97056a295403b79776626ab2
Luke Policinski <Luke@LukePOLO.com>
2024-11-07 09:00:17 -0500

    chore: adding seo

--- app.vue
+++ app.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+import { polyfillCountryFlagEmojis } from "country-flag-emoji-polyfill";
+
+const title = "5Stack - Counter-Strike Management System";
+const description = "A Comprehensive Panel for Managing Servers, Matches, and Tournaments";
+
+useSeoMeta({
+  title: title,
+  ogTitle: title,
+  description: description,
+  ogDescription: description,
+  ogImage: 'https://5stack.gg/_ipx/_/favicon/512.png',
+  twitterCard: description,
+})
+</script>
+
+
 <template>
   <NuxtPwaManifest />
   <NuxtLayout>
@@ -6,8 +23,7 @@
   <Toaster />
 </template>
 
-<script>
-import { polyfillCountryFlagEmojis } from "country-flag-emoji-polyfill";
+<script lang="ts">
 
 export default {
   beforeCreate() {

74365b6be30e9cc678e3c3c243fec57ee6c2aa01
Luke Policinski <Luke@LukePOLO.com>
2024-11-07 09:57:04 -0500

    chore: more seto attempts

--- app.vue
+++ app.vue
@@ -3,24 +3,37 @@ import { polyfillCountryFlagEmojis } from "country-flag-emoji-polyfill";
 
 const title = "5Stack | Counter-Strike Management System";
 const description = "A Comprehensive Panel for Managing Servers, Matches, and Tournaments";
+const url = `https://${useRuntimeConfig().public.demosDomain}`
 
 useSeoMeta({
   title: title,
-  ogTitle: title,
   description: description,
-  ogDescription: description,
-  ogImage: 'https://5stack.gg/_ipx/_/favicon/512.png',
-  twitterCard: 'summary_large_image',
+  siteName: '5Stack',
+
+  ogLocale: 'en',
+  ogType: 'website',
+  
+  ogTitle: title,
+  ogSiteName: '5Stack',
+
+  ogUrl: url,
+  ogImage: `${url}/_ipx/_/favicon/512.png`,
 })
 
 useServerSeoMeta({
   robots: 'index, follow',
   title: title,
-  ogTitle: title,
   description: description,
-  ogDescription: description,
-  ogImage: 'https://5stack.gg/_ipx/_/favicon/512.png',
-  twitterCard: 'summary_large_image',
+  siteName: '5Stack',
+
+  ogLocale: 'en',
+  ogType: 'website',
+  
+  ogTitle: title,
+  ogSiteName: '5Stack',
+
+  ogUrl: url,
+  ogImage: `${url}/_ipx/_/favicon/512.png`,
 })
 </script>
 

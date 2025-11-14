253a234cc03dc503017a25f0250e9e1d122773e8
Luke Policinski <luke@lukepolo.com>
2025-11-14 09:56:35 -0500

    chore: be more aggresive on type checking

--- utilities/formatBytes.ts
+++ utilities/formatBytes.ts
@@ -1,11 +1,17 @@
 export default function formatBytes(Bytes: number) {
-  if (Bytes === 0 || isNaN(Bytes)) {
+  if (!Bytes) {
+    return "0 B";
+  }
+
+  const _bytes = typeof Bytes === "string" ? parseInt(Bytes) : Bytes;
+
+  if (Bytes === 0 || isNaN(_bytes)) {
     return "0 B";
   }
 
   const units = ["B", "KB", "MB", "GB", "TB", "PB"];
-  const index = Math.floor(Math.log(Bytes) / Math.log(1000));
-  const value = Bytes / Math.pow(1000, index);
+  const index = Math.floor(Math.log(_bytes) / Math.log(1000));
+  const value = _bytes / Math.pow(1000, index);
 
   return `${value.toFixed(2)} ${units[index]}`;
 }

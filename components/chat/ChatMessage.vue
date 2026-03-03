ed8122ef4f50d88b5cb90779699d76b998be106f
Luke Policinski <Luke@LukePOLO.com>
2026-03-03 17:33:52 -0500

    chore: fix chat alignment and disable auto focus for mobile (#233)

--- components/chat/ChatMessage.vue
+++ components/chat/ChatMessage.vue
@@ -4,43 +4,48 @@ import PlayerDisplay from "~/components/PlayerDisplay.vue";
 </script>
 
 <template>
-  <div class="my-1.5 text-[11px] leading-snug">
-    <div class="grid grid-cols-[40px_1fr] gap-x-1.5 items-start">
-      <div class="mt-0.5 mx-2">
-        <PlayerDisplay
-          :player="message.from"
-          size="sm"
-          :compact="true"
-          :align-top="true"
-          :show-online="false"
-          :show-elo="false"
-          :show-steam-id="false"
-          :show-add-friend="false"
-          :tooltip="false"
-          :linkable="false"
-          :show-name="false"
-          :show-flag="false"
-          :show-role="false"
-          v-if="!isSameSender || !isCloseTogether"
-        />
-      </div>
+  <div
+    :class="[
+      'relative pl-12 text-[11px] leading-snug',
+      isSameSender && isCloseTogether ? 'mt-0.5 mb-0.5' : 'my-1.5',
+    ]"
+  >
+    <div
+      v-if="showMeta"
+      class="absolute left-2 top-0"
+    >
+      <PlayerDisplay
+        :player="message.from"
+        size="sm"
+        :compact="true"
+        :align-top="true"
+        :show-online="false"
+        :show-elo="false"
+        :show-steam-id="false"
+        :show-add-friend="false"
+        :tooltip="false"
+        :linkable="false"
+        :show-name="false"
+        :show-flag="false"
+        :show-role="false"
+      />
+    </div>
 
-      <div>
-        <div
-          class="flex items-center space-x-1.5 text-muted-foreground text-[10px]"
-          v-if="!isSameSender || !isCloseTogether"
-        >
-          <h4 class="font-semibold truncate max-w-[140px]">
-            {{ message.from.name }}
-          </h4>
-          <span class="text-[10px] whitespace-nowrap">
-            <time-ago :date="message.timestamp"></time-ago>
-          </span>
-        </div>
-        <p class="text-[11px] leading-snug break-words">
-          {{ message.message }}
-        </p>
+    <div>
+      <div
+        v-if="showMeta"
+        class="flex items-center space-x-1.5 text-muted-foreground text-[10px]"
+      >
+        <h4 class="font-semibold truncate max-w-[140px]">
+          {{ message.from.name }}
+        </h4>
+        <span class="text-[10px] whitespace-nowrap">
+          <time-ago :date="message.timestamp"></time-ago>
+        </span>
       </div>
+      <p class="text-[11px] leading-snug break-words">
+        {{ message.message }}
+      </p>
     </div>
   </div>
 </template>
@@ -74,6 +79,9 @@ export default {
       messageTimestamp.setMinutes(messageTimestamp.getMinutes() - 5);
 
       return previousTimestamp > messageTimestamp;
+    },
+    showMeta() {
+      return !this.isSameSender || !this.isCloseTogether;
     },
   },
 };

866f0d204a5fa14bb6da723736c0eee56335a840
Leonardo Bujas <leonardo@locastic.com>
2026-03-06 14:07:58 +0100

    Fix excessive notifications on match start/end (#251)
    Co-authored-by: Flegma <Flegma@users.noreply.github.com>

--- layouts/components/AppNotifications.vue
+++ layouts/components/AppNotifications.vue
@@ -4,7 +4,6 @@ import { Sheet, SheetContent, SheetTrigger } from "~/components/ui/sheet";
 import { Button } from "~/components/ui/button";
 import TimeAgo from "~/components/TimeAgo.vue";
 import TeamInviteNotification from "~/components/TeamInviteNotification.vue";
-import { playerFields } from "~/graphql/playerFields";
 </script>
 
 <template>
@@ -188,180 +187,13 @@ import { playerFields } from "~/graphql/playerFields";
 </template>
 
 <script lang="ts">
-import { typedGql } from "~/generated/zeus/typedDocumentNode";
-import { $, order_by } from "~/generated/zeus";
 import { generateMutation } from "~/graphql/graphqlGen";
 
 export default {
   data() {
     return {
       isOpen: false,
-      team_invites: [],
-      tournament_team_invites: [],
-      notifications: [] as Array<{
-        id: string;
-        title: string;
-        message: string;
-        steam_id: string;
-        type: string;
-        entity_id: string;
-        is_read: boolean;
-        created_at: string;
-        actions?: Array<{
-          graphql: {
-            type: string;
-            action: string;
-            selection: Record<string, any>;
-            variables?: Record<string, any>;
-          };
-        }>;
-      }>,
     };
-  },
-  apollo: {
-    $subscribe: {
-      team_invites: {
-        query: typedGql("subscription")({
-          team_invites: [
-            {
-              order_by: [
-                {},
-                {
-                  created_at: order_by.desc,
-                },
-              ],
-              where: {
-                steam_id: {
-                  _eq: $("steam_id", "bigint!"),
-                },
-              },
-            },
-            {
-              id: true,
-              team: {
-                id: true,
-                name: true,
-              },
-              invited_by: {
-                ...playerFields,
-              },
-              created_at: true,
-            },
-          ],
-        }),
-        variables: function () {
-          return {
-            steam_id: this.me.steam_id,
-          };
-        },
-        result({ data }: { data: any }) {
-          this.team_invites = data.team_invites;
-        },
-      },
-      tournament_team_invites: {
-        query: typedGql("subscription")({
-          tournament_team_invites: [
-            {
-              order_by: [
-                {},
-                {
-                  created_at: order_by.desc,
-                },
-              ],
-              where: {
-                steam_id: {
-                  _eq: $("steam_id", "bigint!"),
-                },
-              },
-            },
-            {
-              id: true,
-              team: {
-                id: true,
-                name: true,
-                tournament: {
-                  name: true,
-                },
-              },
-              invited_by: {
-                ...playerFields,
-              },
-              created_at: true,
-            },
-          ],
-        }),
-        variables: function () {
-          return {
-            steam_id: this.me.steam_id,
-          };
-        },
-        result({ data }: { data: any }) {
-          this.tournament_team_invites = data.tournament_team_invites;
-        },
-      },
-      notifications: {
-        query: typedGql("subscription")({
-          notifications: [
-            {
-              order_by: [
-                {},
-                {
-                  created_at: order_by.desc,
-                },
-              ],
-              where: {
-                _and: [
-                  {
-                    deleted_at: {
-                      _is_null: true,
-                    },
-                  },
-                  {
-                    _or: [
-                      {
-                        is_read: {
-                          _eq: false,
-                        },
-                      },
-                      {
-                        _and: [
-                          {
-                            is_read: {
-                              _eq: true,
-                            },
-                          },
-                          {
-                            created_at: {
-                              _gt: new Date(
-                                Date.now() - 7 * 24 * 60 * 60 * 1000,
-                              ),
-                            },
-                          },
-                        ],
-                      },
-                    ],
-                  },
-                ],
-              },
-            },
-            {
-              id: true,
-              title: true,
-              message: true,
-              steam_id: true,
-              type: true,
-              entity_id: true,
-              is_read: true,
-              created_at: true,
-              actions: true,
-            },
-          ],
-        }),
-        result({ data }: { data: any }) {
-          this.notifications = data.notifications;
-        },
-      },
-    },
   },
   watch: {
     $route() {
@@ -469,23 +301,17 @@ export default {
     },
   },
   computed: {
-    me() {
-      return useAuthStore().me;
+    notifications() {
+      return useNotificationStore().notifications;
+    },
+    team_invites() {
+      return useNotificationStore().team_invites;
+    },
+    tournament_team_invites() {
+      return useNotificationStore().tournament_team_invites;
     },
     hasNotifications() {
-      if (this.team_invites.length > 0) {
-        return true;
-      }
-
-      if (this.tournament_team_invites.length > 0) {
-        return true;
-      }
-
-      return (
-        this.notifications.filter((notification) => {
-          return notification.is_read === false;
-        }).length > 0
-      );
+      return useNotificationStore().hasNotifications;
     },
   },
 };
